d8833725b916465cb5778ff0a07284a835692bb4
Uppercase <serhiohejo@gmail.com>
2021-10-27 12:53:18 +0300

    html wqithout adaptivity

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,18 @@
 import '~/app/libs/jquery/jquery.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+	$(window).on('resize',function(){
+		if(screen.width>=1200){
+            $(".first_screen").css("min-height",(window.innerHeight-$("header").outerHeight())*1.3)
+		}else if(screen.width>=992){
+            $(".first_screen").css("min-height",(window.innerHeight-$("header").outerHeight()))
+		}
 
-	$(".first_screen").css("min-height",window.innerHeight-$("header").outerHeight())
-
+    })
+    $(window).trigger('resize')
+	$(document).on("click",".product .variations .item",function (e) {
+		e.preventDefault()
+		$(this).parents(".product").find(".variations .item").removeClass("active")
+		$(this).addClass("active")
+    })
 })
